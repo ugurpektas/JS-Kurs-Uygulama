2da946b8ef1b5b11a17a5e5cd48e7fe9053caf59
UĞUR PEKTAŞ <76955236+ugurpektas@users.noreply.github.com>
2021-04-18 00:22:51 +0300

    kurs bilgileri tabloya ekleme işlemi yapıldı

--- appES5.js
+++ appES5.js
@@ -6,8 +6,28 @@ function Course (title,instructor,image){
 }
 
 // UI Constructer
-function UI(){
+function UI (){
 
+}
+
+UI.prototype.addCourseToList = function(course){
+    const list = document.getElementById('course-list');
+
+    var html = `
+        <tr>
+            <td><img src="img/${course.image}"></img></td>
+            <td>${course.title}</td>
+            <td>${course.instructor}</td>
+            <td><a href="#" class="btn btn-danger btn-sm">Delete</a></td>
+        </tr>
+    `;
+    list.innerHTML += html;
+}
+
+UI.prototype.clearControls = function () {
+    const title = document.getElementById('title').value="";
+    const instructor = document.getElementById('instructor').value="";
+    const image = document.getElementById('image').value="";
 }
 
 document.getElementById('new-course').addEventListener('submit',function(e){
@@ -21,10 +41,13 @@ document.getElementById('new-course').addEventListener('submit',function(e){
 
     console.log(course);
     //save to database
-    
+    const ui = new UI();
 
-    // show on the ui
+    // ADD course to list
+    ui.addCourseToList(course);
 
+    // clear controls
+    ui.clearControls();
 
 
 
